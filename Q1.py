cebbcedfefbe88c1bb80cdf13dfc5ee5888b39fe
Jiapeng Wu <jiapengwu@Jiapengs-MacBook-Pro.local>
2019-09-22 18:50:06 -0400

    WIP

--- Q1.py
+++ Q1.py
@@ -8,15 +8,25 @@ from scipy.sparse import csgraph
 from scipy.sparse.linalg import eigs
 import seaborn as sns
 directed_fnames = ['metabolic.edgelist.txt', 'citation.edgelist.txt', 'email.edgelist.txt', 'www.edgelist.txt']
+from collections import Counter
+
 
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument('--file', '-f', type=str, default='protein.edgelist.txt')
+
+    parser.add_argument('--all', action="store_true")
+    parser.add_argument('-a', action="store_true")
+    parser.add_argument('-b', action="store_true")
+    parser.add_argument('-c', action="store_true")
+    parser.add_argument('-d', action="store_true")
+    parser.add_argument('-e', action="store_true")
+    parser.add_argument('-f', action="store_true")
+    parser.add_argument('--file', type=str, default='protein.edgelist.txt')
     args = parser.parse_args()
     return args
 
 
-def load_data(fname):
+def load_data(fname, directed):
     verticies = set()
     edges = set()
 
@@ -35,13 +45,13 @@ def load_data(fname):
     print("Number of edges: {}".format(len(edges)))
 
     if directed:
-        for i,j in edges:
+        for i, j in edges:
             A[i][j] = 1
     else:
-        for i,j in edges:
+        for i, j in edges:
             A[j][i] = A[i][j] = 1
 
-    return A, csr_matrix(A)
+    return A, csr_matrix(A), len(verticies), len(edges)
 
 
 def calc_degree(degrees, title=""):
@@ -56,27 +66,31 @@ def calc_degree(degrees, title=""):
     non_zero_values = values[non_zero_idx]
     log_x = np.log(non_zero_values); log_y = np.log(non_zero_counts)
     z = np.polyfit(log_x, log_y, deg=1)
+    print(z)
     f = np.poly1d(z)
-    plt.title('{} degree distribution'.format(title))
+    plt.title('{}degree distribution'.format(title))
     plt.scatter(log_x, log_y)
     plt.plot(log_x, f(log_x), 'tab:orange')
 
     plt.xlabel('log of Degrees (d), base e')
     plt.ylabel('log of Frequency, base e')
-    plt.show()
+    plt.savefig(os.path.join(out_folder, '{}degree distribution'.format(title)))
+    plt.clf()
+
+    with open(os.path.join(out_folder, '{}slope.txt'.format(title)), "w") as f:
+        f.write("slope: {}".format(z[0]))
 
 
 def plot_degree_distribution():
     if not directed:
         degrees = np.sum(A, axis=1).astype(int)
-
         calc_degree(degrees)
     else:
         out_degrees = np.sum(A, axis=1).astype(int)
         in_degrees = np.sum(A, axis=0).astype(int)
 
-        calc_degree(out_degrees, "out")
-        calc_degree(in_degrees, "in")
+        calc_degree(out_degrees, "out ")
+        calc_degree(in_degrees, "in ")
 
 
 def plot_clustering_coeff():
@@ -92,39 +106,53 @@ def plot_clustering_coeff():
     mask = np.isfinite(cs)
     cs = cs[mask]
     degrees = degrees[mask]
-    cc, count = np.unique(cs, return_counts=True)
-
     plt.title('Clustering coefficient distribution')
-    plt.scatter(cc, count)
+    plt.hist(cs, log=True, bins=50)
 
     plt.xlabel('Clustering coefficient')
-    plt.ylabel('Frequency')
-    plt.show()
+    plt.ylabel('Frequency(log scale)')
+    plt.savefig(os.path.join(out_folder, 'Clustering coefficient distribution'))
+    plt.clf()
 
-    print("Average clustering coefficient: {}".format(np.mean(cs)))
+    with open(os.path.join(out_folder, 'avg_clustering_coeff.txt'), "w") as f:
+        f.write("Average clustering coefficient: {}".format(np.mean(cs)))
     return cs, degrees
 
 
 def plot_shortest_path_dist():
-
     D = csgraph.shortest_path(A_sparse)
     D = D[np.isfinite(D)]
 
+    plt.title('Shortest path lengths distribution')
     unique, counts = np.unique(D.flatten(), return_counts=True)
     plt.scatter(unique[1:], counts[1:])
-    plt.xlabel('Shorted path length')
+    plt.xlabel('Lengths of shorted paths')
     plt.ylabel('Frequency')
-    plt.show()
+    plt.savefig(os.path.join(out_folder, 'Shortest path lengths distribution'))
+    plt.clf()
     average = np.mean(np.mean(D))
-    print("Average distance: {}".format(average))
+
+    with open(os.path.join(out_folder, 'avg_shortest_path_distance.txt'), "w") as f:
+        f.write("Average shortest path distance: {}".format(average))
     return D
 
 
 def plot_connected_components():
     n_cc, ccs = csgraph.connected_components(A_sparse)
-    _, counts = np.unique(ccs, return_counts=True)
-    pdb.set_trace()
-    print("Number of CC's: {}. Proportion of nodes in GCC: {}.".format(n_cc, np.max(counts)/node_size))
+
+    values, counts = np.unique(ccs, return_counts=True)
+    max_count = np.max(counts)
+
+    plt.title('Connected components distribution')
+    plt.scatter(range(1, len(counts) + 1), counts/node_count, s=2)
+
+    plt.xlabel("Connected components")
+    plt.ylabel("Proportions of nodes in each CC")
+    plt.savefig(os.path.join(out_folder, 'connected components distribution'))
+    plt.clf()
+
+    with open(os.path.join(out_folder, 'connected_components.txt'), "w") as f:
+        f.write("Number of CC's: {}. Proportion of nodes in GCC: {}.".format(n_cc, max_count / node_count))
 
 
 def plot_spectral_gap():
@@ -132,15 +160,18 @@ def plot_spectral_gap():
     L = D - A
     # eigenvalues and eigenvectors
     vals, vecs = eigs(csr_matrix(L))
-    pdb.set_trace()
     vals = vals[np.argsort(vals)]
-    plt.scatter(range(len(vals)), vals, s=2)
+
+    plt.title('Eigenvalue distribution distribution')
+    plt.scatter(range(1, len(vals) + 1), vals, s=5)
 
     plt.ylabel('eigenvalue')
-    plt.show()
+    plt.savefig(os.path.join(out_folder, 'eigenvalue distribution distribution'))
+    plt.clf()
 
     spectral_gap = vals[vals != 0][0]
-    print("Spectral gap: {}".format(spectral_gap))
+    with open(os.path.join(out_folder, 'spectral_gap.txt'), "w") as f:
+        f.write("Spectral gap: {}".format(spectral_gap))
     return spectral_gap
 
 
@@ -153,11 +184,9 @@ def plot_degree_correlation():
 
     values = np.unique(degrees)
     max_degree = int(np.max(values))
-
-
     DC = np.zeros((max_degree+1, max_degree+1))
 
-    for i in range(node_size):
+    for i in range(node_count):
         N_i = A[i].nonzero()[0]
         ki = degrees[i]
         for j in N_i:
@@ -165,50 +194,66 @@ def plot_degree_correlation():
             DC[ki][kj] += 1
     DC /= edge_count
 
-
+    plt.title('Degree correlations distribution')
     with sns.axes_style("white"):
         ax = sns.heatmap(DC, cmap=sns.cm.rocket_r)
         ax.invert_yaxis()
-        plt.show()
-    return degrees
+    plt.savefig(os.path.join(out_folder, 'eigenvalue distribution distribution'))
+    plt.clf()
+
 
 def plot_degree_clustering(cc, degrees):
-    cc = 100*cc
     max_degree = int(np.max(np.unique(degrees)))
     max_cc = int(np.max(np.unique(cc)))
-    # DCC = np.zeros((max_degree+1, max_cc+1))
-    # for i in range(len(degrees)):
-    #     ki = int(degrees[i])
-    #     cci = int(cc[i])
-    #     DCC[ki, cci] += 1
+    DCC = np.zeros((max_degree+1, max_cc+1))
 
-    heatmap, xedges, yedges = np.histogram2d(cc, degrees, bins=50)
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    for i in range(len(degrees)):
+        ki = int(degrees[i])
+        cci = int(cc[i])
+        DCC[ki, cci] += 1
 
+    zipped = list(zip(cc, degrees))
+
+    cnt = Counter(zipped)
+    values = cnt.keys()  # equals to list(set(words))
+    counts = list(cnt.values())
+    x = list(map(lambda x: x[0], values))
+    y = list(map(lambda x: x[1], values))
+
+    plt.title('Degree-clustering coefficient distribution')
+    plt.scatter(x, y, s=counts)
+
+    plt.xlabel("Degree")
+    plt.ylabel("Clustering coefficients")
+    plt.savefig(os.path.join(out_folder, 'degree clustering coeff distribution'))
     plt.clf()
-    plt.imshow(heatmap.T, extent=extent, origin='lower')
-    plt.show()
 
-    # with sns.axes_style("white"):
-    #     ax = sns.heatmap(DCC, cmap=sns.cm.rocket_r)
-    #     ax.invert_yaxis()
-    #     plt.show()
 
 if __name__ == '__main__':
     args = get_args()
     fname = os.path.join('networks', args.file)
     directed = args.file in directed_fnames
-    A, A_sparse = load_data(fname)
+    A, A_sparse, node_count, edge_count = load_data(fname, directed)
     if directed:
         A_undirected = np.maximum(A, A.transpose())
         A_undirected_sparse = csr_matrix(A_undirected)
 
-    node_size = A.shape[0]
-    # plot_degree_distribution()
-    cc, degrees = plot_clustering_coeff()
-    # plot_shortest_path_dist()
-    # plot_connected_components()
-    # plot_spectral_gap()
-    # plot_degree_correlation()
-    plot_degree_clustering(cc, degrees)
-    
+    out_folder = os.path.join("results", args.file.split(".")[0])
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+
+    if args.all:
+        args.a = args.b = args.c = args.d = args.e = args.f = True
+    if args.a:
+        plot_degree_distribution()
+    if args.b:
+        cc, degrees = plot_clustering_coeff()
+        plot_degree_clustering(cc, degrees)
+    if args.c:
+        plot_shortest_path_dist()
+    if args.d:
+        plot_connected_components()
+    if args.e:
+        plot_spectral_gap()
+    if args.f:
+        plot_degree_correlation()
---
+++ Q2.py
@@ -0,0 +1,39 @@
+from Q1 import *
+
+
+def init_graph():
+    degrees = np.zeros(node_count)
+    A_new = np.zeros((node_count, node_count))
+    degrees[0] = 2
+    A_new[0][0] += 1
+    return degrees, A_new
+
+
+def add_node(t):
+    sum_degrees = 2 + (2*t-1)*m
+    distribution = np.append(degrees[:t]/sum_degrees, m/sum_degrees)
+    target_nodes = np.random.choice(range(t+1), m, replace=False, p=distribution)
+    degrees[t] += m
+    for target in target_nodes:
+        degrees[target] += 1
+        A_new[t][target] += 1
+        if not target == t:
+            A_new[target][t] += 1
+
+
+def add_nodes():
+    for t in range(1, node_count):
+        add_node(t)
+
+if __name__ == '__main__':
+    args = get_args()
+    fname = os.path.join('networks', args.file)
+    out_fname = "{}".format(args.file.split('.')[0])
+    directed = args.file in directed_fnames
+    A, A_sparse, node_count, edge_count = load_data(fname, directed)
+    m = int(edge_count/node_count)
+    print(m)
+    degrees, A_new = init_graph()
+    add_nodes()
+    print("Number of edges: {}".format(sum(degrees)/2))
+    np.save(os.path.join('generated', out_fname), A_new)
