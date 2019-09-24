af9db57a6235fb87758bc88f434bdeda8d3c46a3
Jiapeng Wu <jiapeng.wu@mail.mcgill.ca>
2019-09-24 16:16:27 -0400

    WIP

--- generation.py
+++ generation.py
@@ -1,21 +1,25 @@
 from Q1_sparse import *
 import scipy
 
+
 def init_graph():
     degrees = np.zeros(node_count)
     A_new = csr_matrix((node_count, node_count))
-
+    sum_degrees = 0
     for i in range(m0):
         degrees[i] = 2
+        sum_degrees += 2
         A_new[i, i] += 1
-    return degrees, A_new
+    return degrees, A_new, sum_degrees
 
 
 def add_node(t):
-    sum_degrees = 2*m0 + (2*(t-m0)+1)*m
-    distribution = np.append(degrees[:t]/sum_degrees, m/sum_degrees)
-    target_nodes = np.random.choice(range(t+1), m, replace=False, p=distribution)
-    degrees[t] += m
+    global sum_degrees
+    cur_m = m + np.random.choice([0,1], p=[p0, p1])
+    distribution = np.append(degrees[:t], cur_m)/(sum_degrees + cur_m)
+    sum_degrees += 2 * cur_m
+    target_nodes = np.random.choice(range(t+1), cur_m, replace=False, p=distribution)
+    degrees[t] += cur_m
     for target in target_nodes:
         degrees[target] += 1
         A_new[t, target] += 1
@@ -38,10 +42,12 @@ if __name__ == '__main__':
         A_sparse, A_undirected_sparse, node_count, edge_count = load_data(fname, directed)
     else:
         A_sparse, node_count, edge_count = load_data(fname, directed)
-    m = int(edge_count/node_count)
-    m0 = m - 1
+    m0 = m = int(edge_count/node_count)
+    offset = int(edge_count % node_count)
+    p1 = offset/node_count
+    p0 = 1 - p1
     print("m: {}, m0: {}".format(m, m0))
-    degrees, A_new = init_graph()
+    degrees, A_new, sum_degrees = init_graph()
     add_nodes()
 
     if not os.path.exists('generated'):
