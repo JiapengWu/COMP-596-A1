e6916de6abf717891a71ce852ca799eabab4259d
Jiapeng Wu <jwu558@beluga4.int.ets1.calculquebec.ca>
2019-09-24 15:00:19 -0400

    WIP

---
+++ launcher_8h.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-bengioy
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+#SBATCH --mail-type=ALL
+#SBATCH --time=08:00:00
+###########################
+
+bash
+source ~/.bashrc
+source "/home/jwu558/anaconda3/etc/profile.d/conda.sh"
+set -ex
+conda activate rare_entity_3.7
+echo $(date '+%Y_%m_%d_%H_%M') - $SLURM_JOB_NAME - $SLURM_JOBID - `hostname`
+$@
